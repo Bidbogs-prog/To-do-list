ad19ee6f84526c9599f7b11a3b9d4890e2229cc1
Bidbogs-prog <haythamchhilif@gmail.com>
2022-02-02 23:24:58 +0100

    can crosss finished todos

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+
+  e.target.classList.toggle("text-decoration-line-through");
 });
 
 //filtering todos using search bar///
